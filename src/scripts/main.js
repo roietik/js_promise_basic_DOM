2322bddafe830219700145fd1538ea6a22d4df27
Radek <radoslaw.grzymala@hotmail.com>
2025-03-19 14:25:27 +0100

    Feat: Created second promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,3 +19,21 @@ promise1
   .catch((error) => {
     throw error;
   });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise 2 rejected after 3 seconds'));
+  }, 3000);
+});
+
+promise2
+  .then(() => {
+    // Promise 2 resolved, which should not happen in this case
+  })
+  .catch(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message error-message';
+    messageDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(messageDiv);
+  });
