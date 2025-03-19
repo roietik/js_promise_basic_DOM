bcd1e7b4162aad457e6392ee1c588f5d56ccb382
Radek <radoslaw.grzymala@hotmail.com>
2025-03-19 14:24:17 +0100

    Feat: Created first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1
+  .then(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message';
+    messageDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(messageDiv);
+  })
+  .catch((error) => {
+    throw error;
+  });
